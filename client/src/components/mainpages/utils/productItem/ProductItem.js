a98f255058dada97f4747b220d0aaf3dc47a2e22
Djibba <djibba16084@gmail.com>
2023-04-04 06:24:00 +0000

    admin item

--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -2,14 +2,14 @@ import React, {useContext} from 'react'
 import { Link } from 'react-router-dom';
 import { GlobalState } from '../../../../GlobalState';
 
-function ProductItem({product}) {
+function ProductItem({product, isAdmin}) {
 
   return (
     <div className='product_card'>
 
-    {/* {
-      isAdmin && <input type="checkbox" checked={product.checked} />
-    } */}
+      {
+        isAdmin && <input type="checkbox" checked={product.checked} />
+      }
 
       <img src={product.images.url} alt='' />
 
@@ -20,8 +20,20 @@ function ProductItem({product}) {
       </div>
 
       <div className='row_btn'>
-        <Link id='btn_buy' to='#'>Acheter</Link>
-        <Link id='btn_view' to={`/detail/${product._id}`}>Voir</Link>
+
+        {
+          isAdmin ? 
+            <>
+              <Link id='btn_buy' to='#'>Supprimer</Link>
+              <Link id='btn_view' to={`/edit_product/${product._id}`}>Modifier</Link>
+            </> :
+            <>
+              <Link id='btn_buy' to='#'>Acheter</Link>
+              <Link id='btn_view' to={`/detail/${product._id}`}>Voir</Link>
+            </>
+        }
+
+        
       </div>
     </div>
   )
