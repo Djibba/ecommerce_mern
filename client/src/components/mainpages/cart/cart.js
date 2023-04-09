6658cc086ae93cab48dda88632f5ef658edae5e6
Djibba <djibba16084@gmail.com>
2023-04-09 23:56:48 +0000

    somes updates

--- client/src/components/mainpages/cart/cart.js
+++ client/src/components/mainpages/cart/cart.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalState } from './../../../GlobalState';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
+    
+    const state = useContext(GlobalState)
+    const [cart] = state.userAPI.cart
+
+    if(cart.length === 0) {
+        return <h2 style={{ textAlign: 'center', fontSize: '5rem' }} >Pas de produits sur le panier.</h2>
+    }
+
   return (
-    <div>cart</div>
+    <div>
+        {
+            cart.map(product => (
+                <div className='detail'>
+                    <img src={product.images.url} alt='' />
+
+                    <div className='box-detail'>
+                        <div className='row'>
+                            <h2>{product.title}</h2>
+                            <h6>{product.product_id}</h6>
+                        </div>
+                        <span>{product.price} FCFA</span>
+                        <p>{product.description}</p>
+                        <p>{product.content}</p>
+                        <p>Solde: {product.sold}</p>
+                        <Link to="/cart" className='cart'>Acheter Maintenant</Link>
+                    </div>
+                </div>
+            ))
+        }
+    </div>
   )
 }
